735d6a867af0b805096d220b360aae2ba913f969
Stella Beemer <stella.beemer@gmail.com>
2021-04-10 13:11:05 -0700

    what is a pull request

---
+++ graph.js
@@ -0,0 +1,11 @@
+/*
+    graph class for 447 network project
+    based on an adjacency matrix graph representation
+*/
+
+class Graph {
+
+    constructor(vertexNum) {
+        this.vertexNum = vertexNum;
+    }
+};
